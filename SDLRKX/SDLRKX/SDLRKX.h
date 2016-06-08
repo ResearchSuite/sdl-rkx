5a7ad9b635211aee6cb8310719b4f3fe3d5d74d6
James <james.kizer@gmail.com>
2016-06-08 00:10:30 -0400

    Added project file for submodule style projects

---
+++ SDLRKX/SDLRKX/SDLRKX.h
@@ -0,0 +1,19 @@
+//
+//  SDLRKX.h
+//  SDLRKX
+//
+//  Created by James Kizer on 6/7/16.
+//  Copyright © 2016 Foundry @ Cornell Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDLRKX.
+FOUNDATION_EXPORT double SDLRKXVersionNumber;
+
+//! Project version string for SDLRKX.
+FOUNDATION_EXPORT const unsigned char SDLRKXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDLRKX/PublicHeader.h>
+
+
