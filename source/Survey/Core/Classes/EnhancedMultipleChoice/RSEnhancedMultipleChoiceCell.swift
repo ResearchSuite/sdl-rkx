6889e5796ff7bb69ae24fb5bd79a746d8e46769a
jdkizer9 <james.kizer@gmail.com>
2017-04-09 23:39:36 -0400

    Accessory view with label seems ok

--- source/Survey/Core/Classes/EnhancedMultipleChoice/RSEnhancedMultipleChoiceCell.swift
+++ source/Survey/Core/Classes/EnhancedMultipleChoice/RSEnhancedMultipleChoiceCell.swift
@@ -13,7 +13,7 @@ protocol RSEnhancedMultipleChoiceCellDelegate {
     func cellAuxiliaryAnswerChanged(_ cell: RSEnhancedMultipleChoiceCell, answer: Any)
 }
 
-open class RSEnhancedMultipleChoiceCell: UITableViewCell {
+open class RSEnhancedMultipleChoiceCell: UITableViewCell, RSEnhancedMultipleChoiceAccessoryViewDelegate {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var checkImageView: UIImageView!
@@ -24,9 +24,14 @@ open class RSEnhancedMultipleChoiceCell: UITableViewCell {
     
     var auxView: UIView?
     
-    var auxViewTopConstraint: NSLayoutConstraint?
-    var auxViewBottomConstraint: NSLayoutConstraint?
+//    var auxViewTopConstraint: NSLayoutConstraint?
+//    var auxViewBottomConstraint: NSLayoutConstraint?
+    var auxViewConstraints:[NSLayoutConstraint]?
     var auxContainerHeight: NSLayoutConstraint?
+    
+    var auxFormItem: ORKFormItem?
+    
+    var auxFormAnswer: Any? = nil
     
     
     @IBOutlet weak var choiceContainer: UIView!
@@ -59,11 +64,74 @@ open class RSEnhancedMultipleChoiceCell: UITableViewCell {
             return
         }
         
-        
+        self.auxFormItem = auxItem
         
     }
-
     
+    open func updateAuxView(selected: Bool, animated: Bool) {
+        guard let formItem = self.auxFormItem else {
+            return
+        }
+        
+        if selected {
+
+            if let heightConstraint = self.auxContainerHeight {
+                self.auxContainer?.removeConstraint(heightConstraint)
+                self.auxContainerHeight = nil
+            }
+
+            guard self.auxViewConstraints == nil,
+                let auxView = RSEnhancedMultipleChoiceAccessoryView(
+                formItem: formItem,
+                answer: self.auxFormAnswer,
+                maxLabelWidth: self.bounds.width / 2.0,
+                delegate: self
+                ) else { return }
+            
+            if self.auxView == nil,
+                let auxView = RSEnhancedMultipleChoiceAccessoryView(
+                    formItem: formItem,
+                    answer: self.auxFormAnswer,
+                    maxLabelWidth: self.bounds.width / 2.0,
+                    delegate: self
+                ) {
+                self.auxView = auxView
+                self.auxContainer.addSubview(auxView)
+            }
+            
+            
+            print(self.auxContainer)
+            print(self.auxView)
+            
+            let topConstraint = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.auxView, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 0)
+            
+            let bottomConstraint = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: self.auxView, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
+            
+            let widthConstraint = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: self.auxView, attribute: NSLayoutAttribute.width, multiplier: 1, constant: 0)
+            
+            let horizontalCenterConstraint = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self.auxView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+            
+            let constraints = [topConstraint, bottomConstraint, widthConstraint, horizontalCenterConstraint]
+            self.auxContainer?.addConstraints(constraints)
+            self.auxViewConstraints = constraints
+            
+        }
+        else {
+            if let constraints = self.auxViewConstraints { self.auxContainer.removeConstraints(constraints)}
+            self.auxViewConstraints = nil
+            
+//            self.auxView?.removeFromSuperview()
+//            self.auxView = nil
+            
+            let containerHeight = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 0)
+            //this makes it so the box shrinks from the bottom when unselecting
+            containerHeight.priority = 750.0
+            
+            self.auxContainerHeight = containerHeight
+            self.auxContainer.addConstraint(containerHeight)
+            
+        }
+    }
     
     open func updateUI(selected: Bool, animated: Bool) {
         print("updating UI for \(self.textLabel?.text)")
@@ -86,47 +154,62 @@ open class RSEnhancedMultipleChoiceCell: UITableViewCell {
             self.checkImageView.isHidden = false
 //            self.auxContainerHeight.constant = 32
             
-            //only add subview if not already there
-            if self.auxView == nil {
-                if let heightConstraint = self.auxContainerHeight {
-                    self.auxContainer?.removeConstraint(heightConstraint)
-                    self.auxContainerHeight = nil
-                }
-                
-                
-                let auxView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-                self.auxView = auxView
-                self.auxContainer.addSubview(auxView)
-                
-                let topConstraint = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.auxView, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 0)
-                
-                self.auxViewTopConstraint = topConstraint
-                
-                let bottomConstraint = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: self.auxView, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
-                
-                self.auxViewBottomConstraint = bottomConstraint
-                
-                self.auxContainer?.addConstraints([topConstraint, bottomConstraint])
-            }
+//            //only add subview if not already there
+//            if let auxItem = self.auxFormItem,
+//                self.auxView == nil {
+//                
+//                if let heightConstraint = self.auxContainerHeight {
+//                    self.auxContainer?.removeConstraint(heightConstraint)
+//                    self.auxContainerHeight = nil
+//                }
+//
+//                
+//                let auxView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+//                self.auxView = auxView
+//                self.auxContainer.addSubview(auxView)
+//                
+//                let topConstraint = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.auxView, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 0)
+//                
+//                self.auxViewTopConstraint = topConstraint
+//                
+//                let bottomConstraint = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: self.auxView, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
+//                
+//                self.auxViewBottomConstraint = bottomConstraint
+//                
+//                self.auxContainer?.addConstraints([topConstraint, bottomConstraint])
+//            }
             
         }
         else {
             self.titleLabel.textColor = UIColor.black
             self.checkImageView.isHidden = true
 //            self.auxContainerHeight.constant = 0
-            self.auxContainer.removeConstraints([self.auxViewTopConstraint, self.auxViewBottomConstraint].flatMap({$0}))
-            self.auxViewTopConstraint = nil
-            self.auxViewBottomConstraint = nil
-            self.auxView?.removeFromSuperview()
-            self.auxView = nil
-            
-            let containerHeight = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 0)
-            //this makes it so the box shrinks from the bottom when unselecting
-            containerHeight.priority = 750.0
-            
-            self.auxContainerHeight = containerHeight
-            self.auxContainer.addConstraint(containerHeight)
+//            self.auxContainer.removeConstraints([self.auxViewTopConstraint, self.auxViewBottomConstraint].flatMap({$0}))
+//            self.auxViewTopConstraint = nil
+//            self.auxViewBottomConstraint = nil
+//            self.auxView?.removeFromSuperview()
+//            self.auxView = nil
+//            
+//            let containerHeight = NSLayoutConstraint(item: self.auxContainer, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 0)
+//            //this makes it so the box shrinks from the bottom when unselecting
+//            containerHeight.priority = 750.0
+//            
+//            self.auxContainerHeight = containerHeight
+//            self.auxContainer.addConstraint(containerHeight)
         }
+        
+        self.updateAuxView(selected: selected, animated: animated)
     }
+    
+    public func accessoryViewAnswerChanged(accessoryView: RSEnhancedMultipleChoiceAccessoryView, answer: Any?) {
+        self.auxFormAnswer = answer
+    }
+    public func accessoryViewDidBecomeFirstResponder(accessoryView: RSEnhancedMultipleChoiceAccessoryView) {
+        
+    }
+    public func accessoryViewDidResignFirstResponder(accessoryView: RSEnhancedMultipleChoiceAccessoryView) {
+        
+    }
+    
     
 }
---
+++ source/Survey/Core/Classes/RSEnhancedMultipleChoiceAccessoryView.swift
@@ -0,0 +1,74 @@
+//
+//  RSEnhancedMultipleChoiceAccessoryView.swift
+//  Pods
+//
+//  Created by James Kizer on 4/9/17.
+//
+//
+
+import UIKit
+import ResearchKit
+
+public protocol RSEnhancedMultipleChoiceAccessoryViewDelegate: class {
+    func accessoryViewAnswerChanged(accessoryView: RSEnhancedMultipleChoiceAccessoryView, answer: Any?)
+    func accessoryViewDidBecomeFirstResponder(accessoryView: RSEnhancedMultipleChoiceAccessoryView)
+    func accessoryViewDidResignFirstResponder(accessoryView: RSEnhancedMultipleChoiceAccessoryView)
+}
+
+open class RSEnhancedMultipleChoiceAccessoryView: UIView {
+
+    
+    var textLabel: UILabel
+    weak var delegate: RSEnhancedMultipleChoiceAccessoryViewDelegate?
+    
+    public init?(formItem: ORKFormItem,
+         answer: Any?,
+         maxLabelWidth:CGFloat,
+         delegate: RSEnhancedMultipleChoiceAccessoryViewDelegate) {
+        
+        
+        //add label
+        //should be self sizing height
+        //set width to max label width
+        self.textLabel = UILabel()
+        self.textLabel.lineBreakMode = .byWordWrapping
+        self.textLabel.numberOfLines = 0
+        self.textLabel.text = formItem.text
+        
+        let size = self.textLabel.sizeThatFits(CGSize(width: maxLabelWidth, height: CGFloat(MAXFLOAT)))
+        self.textLabel.frame = CGRect(x: 8, y: 16, width: size.width, height: size.height)
+        
+        super.init(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height + 32) )
+        
+        self.addSubview(self.textLabel)
+        print(self.textLabel)
+        
+        let topConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.textLabel, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 16)
+        
+        let bottomConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: self.textLabel, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 16)
+        
+        let leadingContraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: self.textLabel, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 8)
+        
+        let widthConstraint = NSLayoutConstraint(item: self.textLabel, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: maxLabelWidth-8)
+        
+        self.addConstraints([topConstraint, bottomConstraint, leadingContraint])
+        
+        self.textLabel.addConstraint(widthConstraint)
+        
+        //set delegate
+        self.delegate = delegate
+
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+
+open class RSEnhancedMultipleChoiceAccessoryTextBasedView: RSEnhancedMultipleChoiceAccessoryView, UITextFieldDelegate {
+    
+    
+    
+}
